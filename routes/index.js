a54d69369c6fa7df938d966c96636c6f236a9716
bruninrko123 <balves@byupathway.edu>
2025-03-31 19:15:48 -0300

    routes added

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,8 @@ const router = require('express').Router();
 
 router.get('/', (req , res)=>{res.send('School Project');});
 
+router.use("/teachers", require("./teachers"));
+
+router.use("/students", require("./students"))
 
 module.exports = router;
