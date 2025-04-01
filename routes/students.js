2ddf124892e25a18f15e827bf49a0e550d1624a4
Luciano-A-Vilete <luciano.a.vilete@hotmail.com>
2025-04-01 18:13:31 -0300

    updated swagger documentation visuals.

--- routes/students.js
+++ routes/students.js
@@ -4,11 +4,15 @@ const routes = require("express").Router();
 const studentsController = require("../controllers/students");
 const validateStudent = require("../middleware/validation");
 
+/* #swagger.tags = ['Students'] */
 
+// #swagger.summary = 'Get all students'
 routes.get("/", studentsController.getAllStudents);
 
+// #swagger.summary = 'Get student details'
 routes.get("/:id", studentsController.getSingleStudent);
 
+// #swagger.summary = 'Add a new student'
 routes.post("/", validateStudent, studentsController.createStudent);
 
 
--- routes/teachers.js
+++ routes/teachers.js
@@ -2,15 +2,18 @@ const router = require("express").Router();
 const express = require("express");
 
 
-
+/* #swagger.tags = ['Teachers'] */
 const validateTeacher = require("../middleware/validation");
 
-const teachersController = require("../controllers/teachers")
+const teachersController = require("../controllers/teachers");
 
+// #swagger.summary = 'Get all teachers'
 router.get("/", teachersController.getAllteachers);
 
+// #swagger.summary = 'Get teacher details'
 router.get("/:id", teachersController.getSingleTeacher);
 
+// #swagger.summary = 'Add a new teacher'
 router.post("/", validateTeacher, teachersController.createTeacher);
 
 
