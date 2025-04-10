564a2752542b4cef163ca24dde39803080f9870d
NaNajli <nadurodriguez2@gmail.com>
2025-04-10 17:09:01 -0300

    test

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const { getAllBooks, getSingleBook } = require('./books');
+const { getAllClasses, getSingleClass } = require('./classes');
+const { getAllStudents, getSingleStudent } = require('./students');
+const { getAllteachers, getSingleTeacher } = require('./teachers');
+
+module.exports = {
+  getAllBooks,
+  getSingleBook,
+  getAllClasses,
+  getSingleClass,
+  getAllStudents,
+  getSingleStudent,
+  getAllteachers,
+  getSingleTeacher,
+};
