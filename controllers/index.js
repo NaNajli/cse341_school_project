b1624bf55c0cddf9674c35bad14a6d63e9f76a41
NaNajli <nadurodriguez2@gmail.com>
2025-04-11 17:00:48 -0300

    test pass

--- controllers/index.js
+++ controllers/index.js
@@ -16,18 +16,3 @@ module.exports = {
   getSingleTeacher,
 };
 
-const { getAllBooks, getSingleBook } = require('./books');
-const { getAllClasses, getSingleClass } = require('./classes');
-const { getAllStudents, getSingleStudent } = require('./students');
-const { getAllteachers, getSingleTeacher } = require('./teachers');
-
-module.exports = {
-  getAllBooks,
-  getSingleBook,
-  getAllClasses,
-  getSingleClass,
-  getAllStudents,
-  getSingleStudent,
-  getAllteachers,
-  getSingleTeacher,
-};
